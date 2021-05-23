feeac9fa3a8135bc859d88f45b0a9dc433a7bfb3
emirnes <84445885+emirnes@users.noreply.github.com>
2021-05-23 20:57:32 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
 function validate(){
   var name = document.getElementById("name").value;
-  var subject = document.getElementById("subject").value;
   var phone = document.getElementById("phone").value;
   var email = document.getElementById("email").value;
   var message = document.getElementById("message").value;
@@ -10,32 +9,25 @@ function validate(){
   
   var text;
   if(name.length < 5){
-    text = "Please Enter valid Name";
-    error_message.innerHTML = text;
-    return false;
-  }
-  if(subject.length < 10){
-    text = "Please Enter Correct Subject";
+    text = "Lütfen İsminizi Doğru Giriniz";
     error_message.innerHTML = text;
     return false;
   }
   if(isNaN(phone) || phone.length != 10){
-    text = "Please Enter valid Phone Number";
+    text = "Lütfen Geçerli Bir Telefon Numarası Giriniz";
     error_message.innerHTML = text;
     return false;
   }
   if(email.indexOf("@") == -1 || email.length < 6){
-    text = "Please Enter valid Email";
+    text = "Lütfen Geçerli Bir Email Adresi Giriniz";
     error_message.innerHTML = text;
     return false;
   }
   if(message.length <= 140){
-    text = "Please Enter More Than 140 Characters";
+    text = "Ltfen 140 Karakterden Fazla Karakter Kullanınız";
     error_message.innerHTML = text;
     return false;
   }
-  alert("Form Submitted Successfully!");
-
+  alert("Form Başarıyla Gönderildi!");
   return true;
 }
-
